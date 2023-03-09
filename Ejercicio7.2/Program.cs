e44b7c17e0f82add3e7077effb3fbba36504c2c4
Cintia123AB <navacintia1d@gmail.com>
2023-03-08 23:15:18 -0600

    Session 7 Ejecicio7.2

---
+++ Ejercicio7.2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace numerosImpares
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Imprimir numero impares del a al 99");
+
+
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine($"Numero: {i}");
+            }
+        }
+        Console.ReadKey();
+    }
+
+}
+}
