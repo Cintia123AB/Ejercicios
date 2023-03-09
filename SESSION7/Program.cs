112c3f2606a737cf95057da28b61b4d09c3bd095
Cintia123AB <navacintia1d@gmail.com>
2023-03-08 21:54:16 -0600

    Ejercicio7

---
+++ SESSION7/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("SEGUNDA LINEA ");
+
+
+for(int i = 0; i<=10; i++  )
+{
+    Console.WriteLine(i);
+}
+
+int numeroPersonas = 11;
+string NombrePersona = "Cintia Nava ";
+Console.WriteLine("En este room hay {0} personas y una se llama {1}" , numeroPersonas, NombrePersona);
