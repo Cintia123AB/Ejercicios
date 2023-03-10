8796c15a491bd02b9461bb74c459714a210f8777
Cintia123AB <navacintia1d@gmail.com>
2023-03-09 22:22:59 -0600

    session7 ejercicio7.1

---
+++ Ejercicio7.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroabsoluto
+{
+    class Program
+    {
+       static void Main (string[] args)
+    {
+        int numero;
+        
+       Console.Write("Ingresa Un Numero: ");
+       numero = int.Parse(Console.ReadLine());
+    
+    if(numero <  0)
+       Console.Write("El numeor es: " + numero + " y su valor absoluto es:" + Math.Abs(numero));
+    else
+       Console.Write ("El numero es:" + numero);
+
+    Console.ReadKey();
+
+ 
+    }
+}
+}
