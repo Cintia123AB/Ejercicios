b5a263c1ec24f3933c8310c263958a87dd136529
Cintia123AB <navacintia1d@gmail.com>
2023-03-16 01:30:21 -0600

    ejercicio 8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("EJERCICIO 8  ");
+
+sbyte numeroMinimoByteSigno = -20;
+
+
+byte numeroMinimoByteSinSigno = 150;
+
+
+Console.Write(numeroMinimoByteSigno);
+Console.Write("\n");
+Console.Write(numeroMinimoByteSinSigno);
+
+
+numeroMinimoByteSigno = (sbyte) numeroMinimoByteSinSigno;
+Console.Write("\n");
+Console.Write(numeroMinimoByteSigno);
