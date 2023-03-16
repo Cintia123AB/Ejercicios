6f7b431b3b946e358770a821b08022e8aadcffb2
Cintia123AB <navacintia1d@gmail.com>
2023-03-16 01:21:47 -0600

    Ejercicio8.2

---
+++ Ejercicio8.2/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Persona
+{
+
+    public int edad;
+    public void SetAge(int n)
+    {
+        edad = n;
+    }
+    public void Greet()
+    {
+        Console.WriteLine("Persona: Hola!");
+    }
+}
+
+public class Estudiante : Persona
+{
+    public void GoToClasses()
+    {
+        Console.WriteLine("Estudiante: voy a clase .");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("Estudiante: Mi Edad es: " + edad + " años");
+    }
+}
+
+public class Maestro: Persona
+{
+    public void Explain()
+    {
+        Console.WriteLine("Maestro:Comienza la explicación.");
+    }
+}
+
+public class EstudianteAndMaestroTest
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona();
+        persona.Greet();
+
+        Estudiante estudiante = new Estudiante();
+        estudiante.SetAge(21);
+        estudiante.Greet();
+        estudiante.ShowAge();
+        estudiante.GoToClasses();
+
+        Maestro maestro = new Maestro();
+        maestro.SetAge(30);
+        maestro.Greet();
+        maestro.Explain();
+    }
+}
